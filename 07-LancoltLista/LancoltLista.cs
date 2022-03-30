c9ef1c823909fbeaa7ef367803219fe85aca7291
szdanika <danikaszab@gmail.com>
2022-03-30 14:20:38 +0200

    Add project files.

---
+++ 07-LancoltLista/LancoltLista.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_LancoltLista
+{
+    delegate void KiirasHandler(string tartalom);
+    class ListaElem
+    {
+        public ListaElem kov;
+        public SzuperHos tart;
+    }
+    internal class LancoltLista
+    {
+        ListaElem fej;
+        ListaElem e;
+        void HozzaAdas(SzuperHos tartalom, KiirasHandler kh)
+        {
+            KiirasHandler _kh = kh;
+            ListaElem p = fej;
+            ListaElem uj = new ListaElem();
+            uj.tart = p;
+            int i = 0;
+        }
+    }
+}
---
+++ 07-LancoltLista/SzuperHos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_LancoltLista
+{
+    enum Oldal { jo, gonosz, civil}
+    internal class SzuperHos
+    {
+        public SzuperHos(string nev, bool mutans, int ero, int gyorsasag, Oldal old)
+        {
+            Nev = nev;
+            Mutans = mutans;
+            Ero = ero;
+            Gyorsasag = gyorsasag;
+            Old = old;
+        }
+
+        public string Nev { get; private set; }
+        public bool Mutans { get; private set; }
+        public int Ero { get; private set; }
+        public int Gyorsasag { get; private set; }
+        public Oldal Old { get; private set; }
+
+    }
+}
